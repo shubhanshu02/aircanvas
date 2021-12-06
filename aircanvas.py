923c138f7ec1434cfa607f634d3dec8e8d79a403
Diya Agrawal <65338807+diya31ag@users.noreply.github.com>
2021-12-06 06:03:05 -0500

    changed UI of canvas (#1)

--- aircanvas.py
+++ aircanvas.py
@@ -6,29 +6,12 @@ from collections import deque
 import HandTrackingModule as htm
 
 
+
 # default called trackbar function
 
 def setValues(x):
     print('')
 
-
-# Creating the trackbars needed for
-# adjusting the marker colour These
-# trackbars will be used for setting
-# the upper and lower ranges of the
-# HSV required for particular colour
-
-cv2.namedWindow('Color detectors')
-cv2.createTrackbar('Upper Hue', 'Color detectors', 153, 180, setValues)
-cv2.createTrackbar('Upper Saturation', 'Color detectors', 0xFF, 0xFF,
-                   setValues)
-cv2.createTrackbar('Upper Value', 'Color detectors', 0xFF, 0xFF,
-                   setValues)
-cv2.createTrackbar('Lower Hue', 'Color detectors', 64, 180, setValues)
-cv2.createTrackbar('Lower Saturation', 'Color detectors', 72, 0xFF,
-                   setValues)
-cv2.createTrackbar('Lower Value', 'Color detectors', 49, 0xFF,
-                   setValues)
 
 # Giving different arrays to handle colour
 # points of different colour These arrays
@@ -39,15 +22,15 @@ cv2.createTrackbar('Lower Value', 'Color detectors', 49, 0xFF,
 bpoints = [deque(maxlen=1024)]
 gpoints = [deque(maxlen=1024)]
 rpoints = [deque(maxlen=1024)]
-ypoints = [deque(maxlen=1024)]
+vpoints = [deque(maxlen=1024)]
 
 # These indexes will be used to mark position
 # of pointers in colour array
 
-blue_index = 0
+black_index = 0
 green_index = 0
 red_index = 0
-yellow_index = 0
+voilet_index = 0
 
 # The kernel to be used for dilation purpose
 
@@ -56,7 +39,8 @@ kernel = np.ones((5, 5), np.uint8)
 # The colours which will be used as ink for
 # the drawing purpose
 
-colors = [(0xFF, 0, 0), (0, 0xFF, 0), (0, 0, 0xFF), (0, 0xFF, 0xFF)]
+# colors = [(0, 0, 0), (254,5,85), (23,154,0), (0, 40, 255)]
+colors = [(0, 0, 0), (255,0, 0), (0, 255, 0), (0, 0, 255)]
 colorIndex = 0
 
 # Here is code for Canvas setup
@@ -85,86 +69,27 @@ while True:
     # Flipping the frame to see same side of yours
 
     frame = cv2.flip(frame, 1)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
+    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # Getting the updated positions of the trackbar
     # and setting the HSV values
 
-    u_hue = cv2.getTrackbarPos('Upper Hue', 'Color detectors')
-    u_saturation = cv2.getTrackbarPos('Upper Saturation',
-            'Color detectors')
-    u_value = cv2.getTrackbarPos('Upper Value', 'Color detectors')
-    l_hue = cv2.getTrackbarPos('Lower Hue', 'Color detectors')
-    l_saturation = cv2.getTrackbarPos('Lower Saturation',
-            'Color detectors')
-    l_value = cv2.getTrackbarPos('Lower Value', 'Color detectors')
-    Upper_hsv = np.array([u_hue, u_saturation, u_value])
-    Lower_hsv = np.array([l_hue, l_saturation, l_value])
 
-    # Adding the colour buttons to the live frame
-    # for colour access
-
-    frame = cv2.rectangle(frame, (40, 1), (140, 65), (122, 122, 122),
-                          -1)
-    frame = cv2.rectangle(frame, (160, 1), (0xFF, 65), colors[0], -1)
-    frame = cv2.rectangle(frame, (275, 1), (370, 65), colors[1], -1)
-    frame = cv2.rectangle(frame, (390, 1), (485, 65), colors[2], -1)
-    frame = cv2.rectangle(frame, (505, 1), (600, 65), colors[3], -1)
+    frame = cv2.circle(frame,(40,90), 20, (255,255,255),-1)
+    frame = cv2.circle(frame,(40,140), 20, (0,0,0),-1)
+    frame = cv2.circle(frame,(40,190),20,(255,0,0),-1)
+    frame = cv2.circle(frame,(40,240), 20, (0,255,0),-1)
+    frame = cv2.circle(frame,(40,290), 20, (0,0,255),-1)
 
     cv2.putText(
         frame,
-        'CLEAR ALL',
-        (49, 33),
+        'C',
+        (32,94),
         cv2.FONT_HERSHEY_SIMPLEX,
         0.5,
-        (0xFF, 0xFF, 0xFF),
+        (0,0,0),
         2,
         cv2.LINE_AA,
-        )
-
-    cv2.putText(
-        frame,
-        'BLUE',
-        (185, 33),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        0.5,
-        (0xFF, 0xFF, 0xFF),
-        2,
-        cv2.LINE_AA,
-        )
-
-    cv2.putText(
-        frame,
-        'GREEN',
-        (298, 33),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        0.5,
-        (0xFF, 0xFF, 0xFF),
-        2,
-        cv2.LINE_AA,
-        )
-
-    cv2.putText(
-        frame,
-        'RED',
-        (420, 33),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        0.5,
-        (0xFF, 0xFF, 0xFF),
-        2,
-        cv2.LINE_AA,
-        )
-
-    cv2.putText(
-        frame,
-        'YELLOW',
-        (520, 33),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        0.5,
-        (150, 150, 150),
-        2,
-        cv2.LINE_AA,
-        )
+    )
 
     center = None
     # Ifthe contours are formed
@@ -185,57 +110,58 @@ while True:
         # Now checking if the user wants to click on
         # any button above the screen
 
-        if center[1] <= 65:
+        if center[0] <= 60:
 
             # Clear Button
 
-            if 40 <= center[0] <= 140:
+            if 70 <= center[1] <= 110:
                 bpoints = [deque(maxlen=512)]
                 gpoints = [deque(maxlen=512)]
                 rpoints = [deque(maxlen=512)]
-                ypoints = [deque(maxlen=512)]
+                vpoints = [deque(maxlen=512)]
 
-                blue_index = 0
+                black_index = 0
                 green_index = 0
                 red_index = 0
-                yellow_index = 0
+                voilet_index = 0
 
                 paintWindow[67:, :, :] = 0xFF
-            elif 160 <= center[0] <= 0xFF:
-                colorIndex = 0  # Blue
-            elif 275 <= center[0] <= 370:
-                colorIndex = 1  # Green
-            elif 390 <= center[0] <= 485:
-                colorIndex = 2  # Red
-            elif 505 <= center[0] <= 600:
-                colorIndex = 3  # Yellow
+            elif 120 <= center[1] <= 160:
+                colorIndex = 0  # Black
+            elif 170 <= center[1] <= 210:
+                colorIndex = 1  # Voilet
+            elif 220 <= center[1] <= 260:
+                colorIndex = 2  # Green
+            elif 270 <= center[1] <= 310:
+                colorIndex = 3  # Red
         else:
             if colorIndex == 0:
-                bpoints[blue_index].appendleft(center)
+                bpoints[black_index].appendleft(center)
             elif colorIndex == 1:
-                gpoints[green_index].appendleft(center)
+                vpoints[voilet_index].appendleft(center)
             elif colorIndex == 2:
-                rpoints[red_index].appendleft(center)
+                gpoints[green_index].appendleft(center)
             elif colorIndex == 3:
-                ypoints[yellow_index].appendleft(center)
+                rpoints[red_index].appendleft(center)
     else:
 
     # Append the next deques when nothing is
     # detected to avois messing up
 
         bpoints.append(deque(maxlen=512))
-        blue_index += 1
+        black_index += 1
+        vpoints.append(deque(maxlen=512))
+        voilet_index += 1
         gpoints.append(deque(maxlen=512))
         green_index += 1
         rpoints.append(deque(maxlen=512))
         red_index += 1
-        ypoints.append(deque(maxlen=512))
-        yellow_index += 1
+        
 
     # Draw lines of all the colors on the
     # canvas and frame
 
-    points = [bpoints, gpoints, rpoints, ypoints]
+    points = [bpoints, vpoints, gpoints, rpoints]
     for i in range(len(points)):
 
         for j in range(len(points[i])):
@@ -247,16 +173,15 @@ while True:
                     continue
 
                 cv2.line(frame, points[i][j][k - 1], points[i][j][k],
-                         colors[i], 2)
+                         colors[i], 6)
                 cv2.line(paintWindow, points[i][j][k - 1],
-                         points[i][j][k], colors[i], 2)
+                         points[i][j][k], colors[i], 6)
 
     # Show all the windows
 
     cv2.imshow('Tracking', frame)
     cv2.imshow('Paint', paintWindow)
     # cv2.imshow('mask', Mask)
-
     # If the 'q' key is pressed then stop the application
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
